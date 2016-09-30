d278462bc84e00ee09b1aa4cff9be20b337060d4
Rakesh Ranjan Jha <rakeshjha@ufl.edu>
2016-09-30 01:38:13 -0400

    Lab02: Base - testing existing syscalls

--- main.c
+++ main.c
@@ -2,95 +2,36 @@
 
 #include <xinu.h>
 
-pid32 producer_id;
-pid32 consumer_id;
-pid32 timer_id;
+pid32 recver_id;
+pid32 sender_id;
 
-int32 consumed_count = 0;
-const int32 CONSUMED_MAX = 100;
-
-/* Define your circular buffer structure and semaphore variables here */
-/* */
-
-/* Place your code for entering a critical section here */
-void mutex_acquire(sid32 mutex)
+process sender(void)
 {
-	/* */
-}
-
-/* Place your code for leaving a critical section here */
-void mutex_release(sid32 mutex)
-{
-	/* */
-}
-
-/* Place the code for the buffer producer here */
-process producer(void)
-{
-	/* */
+	/* Simple sender: Send an int message */
+	send(recver_id, (('H'<<8)|'i') & 0xFFFF);
 
 	return OK;
 }
 
-/* Place the code for the buffer consumer here */
-process consumer(void)
+process recver(void)
 {
-	/* Every time your consumer consumes another buffer element,
-	 * make sure to include the statement:
-	 *   consumed_count += 1;
-	 * this will allow the timing function to record performance */
-	/* */
+	/* Simple receiver: Receive an int message */
+	umsg32 myMsg = receive();
+	printf("Got message \"%c%c\" from sender\n",((myMsg>>8) & 0xFF), (myMsg & 0xFF));
 
 	return OK;
 }
 
-
-/* Timing utility function - please ignore */
-process time_and_end(void)
-{
-	int32 times[5];
-	int32 i;
-
-	for (i = 0; i < 5; ++i)
-	{
-		times[i] = clktime_ms;
-		yield();
-
-		consumed_count = 0;
-		while (consumed_count < CONSUMED_MAX * (i+1))
-		{
-			yield();
-		}
-
-		times[i] = clktime_ms - times[i];
-	}
-
-	kill(producer_id);
-	kill(consumer_id);
-
-	for (i = 0; i < 5; ++i)
-	{
-		kprintf("TIME ELAPSED (%d): %d\n", (i+1) * CONSUMED_MAX, times[i]);
-	}
-}
 
 process	main(void)
 {
 	recvclr();
 
-	/* Create the shared circular buffer and semaphores here */
-	/* */
+	recver_id = create(recver, 4096, 50, "recver", 0);
+	sender_id = create(sender, 4096, 50, "sender", 0);
 
-	producer_id = create(producer, 4096, 50, "producer", 0);
-	consumer_id = create(consumer, 4096, 50, "consumer", 0);
-	timer_id = create(time_and_end, 4096, 50, "timer", 0);
-
-	resched_cntl(DEFER_START);
-	resume(producer_id);
-	resume(consumer_id);
-	/* Uncomment the following line for part 3 to see timing results */
-	/* resume(timer_id); */
-	resched_cntl(DEFER_STOP);
+	resume(recver_id);
+	resume(sender_id);
 
 	return OK;
 }
